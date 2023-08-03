38ebf7f6b4099afd67d4342eede591ba29a39a68
Dhawan <dhawan.kumar_cs19@gla.ac.in>
2023-08-03 14:32:47 +0530

    added save tab button, updated js  and added manifest file

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
-
-let myLeads=["www.google.com", "www.linkedin.com", "www.facebook.com"];
+let myLeads=[];
 const inputEl=document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-if(leadsFromLocalStorage){
+if(leadsFromLocalStorage){ 
     myLeads = leadsFromLocalStorage;
     renderLeads(myLeads);
 }
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads(myLeads);
+    })
+})
 
 function renderLeads(leads){
     let listItems="";
     for(let i=0; i<leads.length; i++){
         listItems += `<li>
-                         <a target='_blank' href='${leads[i]}'> ${leads[i]} 
-                     </li>`
+                         <a target='_blank' href='${leads[i]}'> ${leads[i]} </a>
+                     </li>
+                     <hr>`
     }
     ulEl.innerHTML = listItems;
 }
